7c1562621d50e380abdf91b57c0faa97ab6f1227
AlexeyYanchenkov <alexeyyanchenkov25@gmail.com>
2025-04-20 23:19:04 +0300

    24 DZ

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'catalog/product_detail.html', {'product': product})
 
 
 def contacts(request):
@@ -6,4 +11,5 @@ def contacts(request):
 
 
 def home(request):
-    return render(request, "catalog/home.html")
+    products = Product.objects.all()
+    return render(request, 'catalog/home.html', {'products': products})
